02f70600dd9c0e6c7e9a2f11dd3acf58cb6e611c
nina1982-ops <ya.nina.82@yandex.ru>
2024-06-22 12:39:36 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+number_of_hours = 1.5
+сourse_name = 'Python'
+time_for_one_homework = number_of_hours/number_of_homework #Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+print('Курс:',сourse_name,',','всего задач:',number_of_homework,',','затрачено часов:',number_of_hours,',', 'среднее время выполнения',time_for_one_homework,'часа.')
